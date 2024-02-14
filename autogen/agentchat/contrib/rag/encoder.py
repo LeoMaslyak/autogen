ab527999269e4906a4ea05277196d91c8ea8237b
Li Jiang <bnujli@gmail.com>
2024-02-14 16:09:09 +0800

    tmp

--- autogen/agentchat/contrib/rag/encoder.py
+++ autogen/agentchat/contrib/rag/encoder.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Union, Callable
 from .datamodel import Document, Query, QueryResults, Vector, Chunk
-from .utils import lazy_import
+from .utils import lazy_import, logger
 
 
 class EmbeddingFunction(ABC):
@@ -35,7 +35,7 @@ class SentenceTransformerEmbeddingFunction(EmbeddingFunction):
         self.sentence_transformer = lazy_import("sentence_transformers", "SentenceTransformer")(model_name)
         if not self.sentence_transformer:
             raise ImportError("Please install sentence_transformers to use SentenceTransformerEmbeddingFunction.")
-        self.dimensions = self.sentence_transformer.encode(["dummy"]).shape[1]
+        self.dimensions = self.sentence_transformer.encode(["hello"]).shape[1]
 
     def __call__(self, inputs: Union[str, List[str]]) -> List[Vector]:
         if isinstance(inputs, str):
@@ -51,6 +51,14 @@ class Encoder:
 
     def __init__(self, embedding_function: EmbeddingFunction = SentenceTransformerEmbeddingFunction()):
         self._embedding_function = embedding_function
+        self._model_name = (
+            embedding_function.model_name if hasattr(embedding_function, "model_name") else embedding_function.__name__
+        )
+        self._dimensions = (
+            embedding_function.dimensions
+            if hasattr(embedding_function, "dimensions")
+            else len(embedding_function(["hello"])[0])
+        )
 
     def encode_chunks(self, chunks: List[Chunk]) -> List[Document]:
         """
@@ -62,13 +70,20 @@ class Encoder:
         Returns:
             A list of Document.
         """
+        if not isinstance(self._embedding_function, Callable):
+            raise ValueError("The embedding function is not callable.")
+        if not isinstance(self._embedding_function, EmbeddingFunction):
+            logger.warning(
+                "The embedding function is not an instance of EmbeddingFunction. Please make sure the embedding "
+                "function accepts a string or a list of strings and returns a list of Vectors."
+            )
         return [
             Document(
                 **chunk.dict(),
                 title_embedding=self._embedding_function(chunk.title)[0] if chunk.title else [],
-                content_embedding=self.embedding_function(chunk.content)[0] if chunk.content else [],
-                embedding_model=self._embedding_function.model_name,
-                dimensions=self._embedding_function.dimensions
+                content_embedding=self._embedding_function(chunk.content)[0] if chunk.content else [],
+                embedding_model=self._model_name,
+                dimensions=self._dimensions
             )
             for chunk in chunks
         ]
@@ -92,3 +107,33 @@ class Encoder:
             new_embedding_function: The new embedding function.
         """
         self._embedding_function = new_embedding_function
+        self._model_name = (
+            new_embedding_function.model_name
+            if hasattr(new_embedding_function, "model_name")
+            else new_embedding_function.__name__
+        )
+        self._dimensions = (
+            new_embedding_function.dimensions
+            if hasattr(new_embedding_function, "dimensions")
+            else len(new_embedding_function(["hello"])[0])
+        )
+
+    @property
+    def model_name(self) -> str:
+        """
+        Get the model name.
+
+        Returns:
+            The model name.
+        """
+        return self._model_name
+
+    @property
+    def dimensions(self) -> int:
+        """
+        Get the dimensions of the vectors.
+
+        Returns:
+            The dimensions of the vectors.
+        """
+        return self._dimensions
